dea324b7fbbe510e5c8bc246452fb6cfcefa4fac
Gonzalez <0291152@students.dadeschools.net>
2018-04-12 11:59:17 -0400

    started trivia logic

---
+++ logic.js
@@ -0,0 +1,22 @@
+var panel = $(" ");
+var countStartNumber = 30;
+
+//Question set
+var questions = [{
+	question: " What game spiked the platformer genre? ", 
+	answers: ["Sonic", "CastleVania", "SuperMario", "Fortnite"],
+	correctAnswer: "SuperMario",
+	image: "assets/SuperMario_400x400.jpg"
+
+},{
+	question: " What company made the DS? ", 
+	answers: ["Sony", "Microsoft", "Nintendo", "Steam"],
+	correctAnswer: "Nintendo",
+	image: "assets/Nintendo Pic.png"
+}
+
+
+
+
+
+]
