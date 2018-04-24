111d44dc8adc7bdea8ce0d74980c275bbfc991a5
Gonzalez <0291152@students.dadeschools.net>
2018-04-24 12:03:23 -0400

    Still have some debuging to do

--- logic.js
+++ logic.js
@@ -75,11 +75,90 @@ var game = {
 			setTimeout(this.nextQuestion, 3 * 1000);
 		}
 
+
+	},
+
+
+
+results: function() {
+
+	clearInterval(window.timer);
+
+	panel.html("<h2> This is how you did!</h2>");
+
+	$("counter-number").html(this.counter);
+
+	panel.append("<h3>Correct Answers: "+ this.correct + "</h3>");
+	panel.append("<h3>Incorrect Answers: "+ this.incorrect + "</h3>");
+	panel.append("<h3>Unanswered:" + (questions.length - (this.incorrect + this.correctAnswer)) + "</h3>");
+	panel.append("<br><button id='start-over'>Start over?</button>");
+
+
+},
+
+clicked: function(e) {
+	clearInterval(window.timer);
+	if ($(e.target).attr("data-name") === questions[this.currentQuestion].correctAnswer) {
+		this.answeredCorrectly();
+	} else {
+		this.answeredCorrectly();
+	}
+},
+
+answeredIncorrectly:function() {
+
+	this.incorrect++; 
+
+	clearInterval(window.timer);
+
+	panel.html("<h2>NOPE!</h2>");
+	panel.append("<h3>The Correct Answer was: " + questions[this.currentQuestion].correctAnswer + "</h3>");
+	panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+	if (this.currentQuestion === questions.length - 1) {
+		setTimeout(this.results.bind(this), 3 * 1000);
+	}else{
+		setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+		}	
+	},
+
+answeredCorrectly: function() {
+
+		clearInterval(window.timer);
+
+		this.correct++; 
+
+	panel.html("<h2>YOU GOT IT!</h2>");
+	
+	panel.append("<img src='" + questions[this.currentQuestion].image + "' />");
+
+	if (this.currentQuestion === questions.length - 1) {
+		setTimeout(this.results.bind(this), 3 * 1000);
+	}else{
+		setTimeout(this.nextQuestion.bind(this), 3 * 1000);
+		}	
+
 	}
 
-}
+	reset: function() {
+		this.currentQuestion = 0
+		this.counter = countStartNumber;
+		this.correct = 0;
+		this.incorrect = 0;
+		this.loadQuestion();
+	}
+
+};
 
 
+// QUICK EVENTS	
+$(document).on("click", "#start-over", game.reset.bind(game));
 
-//Click Events
-$(document).on("click", "#start-over", game.reset)
+$(document).on("click", ".answer-button",function(e) {
+ game.clicked.bind(game)()
+});
+
+$(document).on("click", "#start",function() {
+	$("#sub-wrapper").prepend("<h2>Time Remaining: <span id='counter'>30</span> Seconds </h2>");
+ game.loadQuestion.bind(game)();
+});
